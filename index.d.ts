c621c7e70a219a8b4eefcc88feab2ddd5f7f6ade
Leonardo Donelli <learts92@gmail.com>
2019-03-28 00:01:40 +0100

    Improve type declarations
    Improve the type declarations by adding all the configration
    options and our methods signatures.
    Publish the type declarations on the NPM package.

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,21 @@
 import * as log from 'loglevel'
 
+interface MonkLogOptions {
+    levelFormatter?: (level: log.LogLevelDesc) => any
+    timestampFormatter?: (date: Date) => any
+    nameFormatter?: (name: string) => any
+    template?: string
+}
+
+declare module 'loglevel' {
+    interface DefaultLogger {
+        dateToISOString: (d: Date, milliseconds?: boolean) => string
+        getLogger (name: string, level?: log.LogLevelDesc, options?: MonkLogOptions): log.Logger
+    }
+    interface Logger {
+        configure (options: MonkLogOptions, reset?: boolean): void
+        getLogger (name: string, level?: log.LogLevelDesc, options?: MonkLogOptions): log.Logger
+    }
+}
+
 export = log
