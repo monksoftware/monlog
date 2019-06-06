35ff71eff4482c58336eec1aa8d8c0cc07817ff8
Leonardo Donelli <learts92@gmail.com>
2019-06-06 13:26:39 +0200

    Fix type declarations, upgrade loglevel and use its own declarations
    Since version 1.6.2 loglevel ships with its own typescript type
    declarations, there is no need to require @types/loglevel anymore.
    
    Additionally, fix our own declarations.

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,22 @@
-import * as log from 'loglevel'
+import log = require('loglevel')
 
-interface MonkLogOptions {
+declare var defaultLogger: DefaultLogger
+export = defaultLogger
+export as namespace defaultLogger
+
+declare interface MonkLogOptions {
     levelFormatter?: (level: log.LogLevelDesc) => any
     timestampFormatter?: (date: Date) => any
     nameFormatter?: (name: string) => any
     template?: string
 }
 
-declare module 'loglevel' {
-    interface DefaultLogger {
-        dateToISOString: (d: Date, milliseconds?: boolean) => string
-        getLogger (name: string, level?: log.LogLevelDesc, options?: MonkLogOptions): log.Logger
-    }
-    interface Logger {
-        configure (options: MonkLogOptions, reset?: boolean): void
-        getLogger (name: string, level?: log.LogLevelDesc, options?: MonkLogOptions): log.Logger
-    }
+declare interface DefaultLogger extends log.RootLogger {
+    dateToISOString: (d: Date, milliseconds?: boolean) => string
+    getLogger (name: string, level?: log.LogLevelDesc, options?: MonkLogOptions): Logger
 }
 
-export = log
+declare interface Logger extends log.Logger {
+    configure (options: MonkLogOptions, reset?: boolean): void
+    getLogger (name: string, level?: log.LogLevelDesc, options?: MonkLogOptions): Logger
+}
